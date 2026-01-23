9d6ac9221e683a1426778f2f2dea4eff428ac211
Manasa Prakash <159694825+ManasaPrakash18@users.noreply.github.com>
2026-01-23 21:36:49 +0530

    Update MongoDB connection URL to use environment variable

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Listing = require('../models/listing');
 const initdata=require("./data.js");
 
-const MONGO_URL="mongodb://127.0.0.1:27017/wonderlust"
+const MONGO_URL=process.env.ATLASDB_URL ;
 
 
 main()
