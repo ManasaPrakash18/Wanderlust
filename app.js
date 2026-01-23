7a825e2d5b0e7d15e9ab5234a60a9759fd55cea9
Manasa Prakash <159694825+ManasaPrakash18@users.noreply.github.com>
2026-01-23 21:01:46 +0530

    db deployment done

--- app.js
+++ app.js
@@ -15,15 +15,18 @@ const multer = require("multer");
 const User=require("./models/user.js");
 
 const session=require("express-session");
+const {MongoStore} = require("connect-mongo");
 const flash=require("connect-flash");
 
 const passport= require("passport");
-const LocalStratergy=require("passport-local").Strategy;
+const LocalStrategy=require("passport-local").Strategy;
 
 // requiring routes
 const listingRouter=require("./routes/listing.js");
 const reviewRouter=require("./routes/review.js");
 const userRouter=require("./routes/user.js");
+
+const dbUrl= process.env.ATLASDB_URL;
 
 
 app.engine('ejs', ejsMate);
@@ -33,7 +36,7 @@ main()
     .catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wonderlust');}
+    await mongoose.connect(dbUrl);}
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -45,17 +48,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+// mongo sessions
+const store= MongoStore.create({
+    mongoUrl : dbUrl,
+    crypto:{
+        secret: process.env.SECRET
+    },
+    touchAfter: 24*3600
+});
+
+
+store.on("error",(err)=>{
+    console.log("Error in MOngo Session", err);
+});
+
+
 // sessions
 const sessionOptions={
-    secret:"secret", 
+    store,
+    secret: process.env.SECRET, 
     resave:false, 
     saveUninitialized:true,
     cookie:{
-        exprires: Date.now() + 7*24*60*60*1000,
+        expires: Date.now() + 7*24*60*60*1000,
         maxAge: 7*24*60*60*1000,
         httpOnly: true
     }
 };
+
+
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -63,7 +84,7 @@ app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStratergy(User.authenticate()));
+passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -75,6 +96,10 @@ app.use((req,res,next)=>{
     res.locals.user=req.user;
     res.locals.dest=req.query.search || "";
     next();
+});
+
+app.get("/",async(req,res)=>{
+    res.redirect("/listings");
 });
 
 // using routes
